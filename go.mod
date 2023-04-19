3b80cfa77bd2b48e0e5014584138d138df316cdd
pixel <zheng2580369@gmail.com>
2023-04-20 01:33:00 +0800

    a

---
+++ go.mod
@@ -0,0 +1,15 @@
+module github.com/zxspirit/gogogo
+
+go 1.20
+
+require github.com/cloudflare/cloudflare-go v0.65.0
+
+require (
+	github.com/google/go-querystring v1.1.0 // indirect
+	github.com/hashicorp/go-cleanhttp v0.5.2 // indirect
+	github.com/hashicorp/go-retryablehttp v0.7.2 // indirect
+	github.com/mattn/go-isatty v0.0.18 // indirect
+	golang.org/x/net v0.9.0 // indirect
+	golang.org/x/text v0.9.0 // indirect
+	golang.org/x/time v0.3.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/cloudflare/cloudflare-go"
+)
+
+func main() {
+	// Construct a new API object using a global API key
+	api, err := cloudflare.New(os.Getenv("CLOUDFLARE_API_KEY"), os.Getenv("CLOUDFLARE_API_EMAIL"))
+	// alternatively, you can use a scoped API token
+	// api, err := cloudflare.NewWithAPIToken(os.Getenv("CLOUDFLARE_API_TOKEN"))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Most API calls require a Context
+	ctx := context.Background()
+
+	// Fetch user details on the account
+	u, err := api.UserDetails(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+	// Print user details
+	fmt.Println(u)
+}
